ae80f988f9cef7683d6a08d950fedab5577c4624
vishal-kumar-888 <jonhalexe000@gmail.com>
2026-04-13 00:12:13 +0530

    Refactor DB connection using async/await

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,22 @@
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost:27017/todoapp')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-module.exports = mongoose;
+module.exports = mongoose; */
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/todoapp');
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
