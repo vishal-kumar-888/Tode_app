5a9b18784ce77e2f2cbb0fde662c36b0097442cd
vishal-kumar-888 <jonhalexe000@gmail.com>
2026-04-14 08:52:48 +0530

    add make database connection

--- index.js
+++ index.js
@@ -9,8 +9,17 @@
 
 
 const express = require('express');
+const connectDB = require('./Config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
 
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
